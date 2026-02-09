369af537c140a7263b200ad1c9a39e2b7d30b9bc
name-q <1620206666@qq.com>
2026-02-09 16:42:30 +0800

    feat:主题优化

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  favicons: ['/logo.png'],
   themeConfig: {
     name: 'AeroUi',
     logo: '/logo.png',
